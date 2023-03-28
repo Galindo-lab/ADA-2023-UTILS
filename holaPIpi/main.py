00b0bdf38236f480417ba0fbdb05fd8960e37c64
Luis E. Galindo Amaya <78140218+Galindo-lab@users.noreply.github.com>
2023-03-28 21:30:47 +0000

    teminado

--- holaPIpi/main.py
+++ holaPIpi/main.py
@@ -1,25 +1,35 @@
 import enum
 
 class States(enum.Enum):
-    aguascalientes = 0
-    chiapas = 1
-    guanajuato = 2
-    michoacan = 3
-    Oaxaca = 4
-    puebla = 5
-    tamaulipas = 6
-    yucatan  = 7
+    """
+    Lugares en los nodos
+    """
+    AGUASCALIENTES = 0
+    CHIAPAS = 1
+    GUANAJUATO = 2
+    MICHOACAN = 3
+    OAXACA = 4
+    PUEBLA = 5
+    TAMAULIPAS = 6
+    YUCATAN  = 7
 
 estaciones = [
-    set([States.michoacan, States.Oaxaca, States.puebla]),
-    set([States.chiapas, States.michoacan, States.aguascalientes]),
-    set([States.guanajuato, States.Oaxaca, States.tamaulipas]),
-    set([States.Oaxaca, States.puebla]),
-    set([States.tamaulipas, States.yucatan])
+    set([States.MICHOACAN, States.OAXACA, States.PUEBLA]),
+    set([States.CHIAPAS, States.MICHOACAN, States.AGUASCALIENTES]),
+    set([States.GUANAJUATO, States.OAXACA, States.TAMAULIPAS]),
+    set([States.OAXACA, States.PUEBLA]),
+    set([States.TAMAULIPAS, States.YUCATAN])
 ]
 
-def numero_diferencias(a, b):
-    return len(a - b)
+origen = 3
+resultado = []
+visitadas = estaciones[origen]
 
-origen = 0
-estaciones_visitadas = estaciones[origen]
+for g,i in enumerate(estaciones):
+    if len(visitadas - i) > 0: # numero_diferencias
+        visitadas = visitadas.union(i)
+        resultado.append(g)
+
+print(F"resultado desde la estacion {origen}:", str(resultado))
+for index in resultado:
+    print(estaciones[index])
