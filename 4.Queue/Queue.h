272f52b4259dd2990de6124d0eceae29b008828a
Luis E. Galindo Amaya <78140218+Galindo-lab@users.noreply.github.com>
2023-07-16 12:25:03 -0700

    Queue.h documentado

--- 4.Queue/Queue.h
+++ 4.Queue/Queue.h
@@ -29,55 +29,82 @@
 #include <assert.h>
 
 typedef List Queue;
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include "LinkedList.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+typedef List Queue;
 
 /**
- * @brief 
- * 
+ * @brief Inicializa una cola vacía.
+ *
+ * @param QUEUE Puntero a la cola a inicializar.
  */
 #define QUEUE_INIT(QUEUE) ListInit(STACK);
 
 /**
- * @brief 
- * 
+ * @brief Agrega un elemento a la cola.
+ *
+ * @param QUEUE Puntero a la cola.
+ * @param DATA Elemento a agregar.
  */
 #define QUEUE_ENQUEUE(QUEUE, DATA) LIST_PUSH(QUEUE, DATA)
 
 /**
- * @brief 
- * 
+ * @brief Elimina y devuelve el primer elemento de la cola.
+ *
+ * @param TYPE Tipo de datos de los elementos de la cola.
+ * @param QUEUE Puntero a la cola.
+ * @return El primer elemento de la cola.
  */
 #define QUEUE_DEQUEUE(TYPE, QUEUE) \
     LIST_GET(TYPE, QUEUE, 0);      \
     ListRemove(QUEUE, 0)
 
 /**
- * @brief 
- * 
+ * @brief Verifica si la cola está vacía.
+ *
+ * @param QUEUE Puntero a la cola.
+ * @return 1 si la cola está vacía, 0 en caso contrario.
  */
 #define QUEUE_EMPTY(QUEUE) LIST_EMPTY(QUEUE)
 
 /**
- * @brief 
- * 
+ * @brief Obtiene el número de elementos en la cola.
+ *
+ * @param QUEUE Puntero a la cola.
+ * @return El número de elementos en la cola.
  */
 #define QUEUE_SIZE(QUEUE) LIST_LEN(QUEUE)
 
 /**
- * @brief 
- * 
+ * @brief Obtiene el primer elemento de la cola sin eliminarlo.
+ *
+ * @param TYPE Tipo de datos de los elementos de la cola.
+ * @param QUEUE Puntero a la cola.
+ * @return El primer elemento de la cola.
  */
 #define QUEUE_FRONT(TYPE, QUEUE) LIST_GET(TYPE, QUEUE, 0)
 
 /**
- * @brief 
- * 
+ * @brief Obtiene el último elemento de la cola sin eliminarlo.
+ *
+ * @param TYPE Tipo de datos de los elementos de la cola.
+ * @param QUEUE Puntero a la cola.
+ * @return El último elemento de la cola.
  */
 #define QUEUE_REAR(TYPE, QUEUE) \
     LIST_GET(TYPE, QUEUE, LIST_LEN(QUEUE) - 1)
 
 /**
- * @brief 
- * 
+ * @brief Libera la memoria utilizada por la cola.
+ *
+ * @param QUEUE Puntero a la cola.
  */
 #define QUEUE_FREE(QUEUE) ListFree(QUEUE);
 
